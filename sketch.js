dcb5aadbdbfacd4fb5ae12a46acd17e2c62a7b09
TW-zzdesier <71486261+TW-zzdesier@users.noreply.github.com>
2020-12-18 15:03:57 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://tw-zzdesier.github.io/aifrog10924223/model.json';
 
 
 function preload() {
